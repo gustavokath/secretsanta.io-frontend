4dd6ce6d2e9d718136509a70c3fac1eae8db51ba
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2020-10-30 14:34:05 -0300

    Initial Repository Setup (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [t] = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {t('react')}
         </p>
         <a
           className="App-link"
